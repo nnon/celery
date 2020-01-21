0503cbcdb59fdc946f3e94bf07ce8fee3537b503
nnon <remackinnon@gmail.com>
2020-01-21 22:11:08 +0000

    Fixed backend settings

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 import sqlalchemy
 from celery import Celery
 
-app = Celery('tasks', result_backend='sqla+sqlite:///results.db', broker='sqla+sqlite:///broker.db')
+app = Celery('tasks', backend='db+sqlite:///results.db', broker='sqla+sqlite:///broker.db')
 
 @app.task
 def add(x, y):
